5185780be1870c27c6630d773bb2588303c0beef
Erin <erinedawson@gmail.com>
2019-08-23 14:21:02 -0600

    refactor to demonstrate export on declaration

--- employee.js
+++ employee.js
@@ -11,7 +11,7 @@ let payGrades = {
   seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
 };
 
-const getCadre = function() {
+export const getCadre = function() {
   if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
     return 'entryLevel';
   } else if (Employee.salary >= payGrades.midLevel.minSalary && Employee.salary <= payGrades.midLevel.maxSalary) {
@@ -19,19 +19,19 @@ const getCadre = function() {
   } else return 'seniorLevel';
 }
 
-const calculateTax = function() {
+export const calculateTax = function() {
   return payGrades[getCadre()].taxMultiplier * Employee.salary;
 }
 
-const getBenefits = function() {
+export const getBenefits = function() {
   return payGrades[getCadre()].benefits.join(', ');
 }
 
-const calculateBonus = function() {
+export const calculateBonus = function() {
   return .02 * Employee.salary;
 }
 
-const reimbursementEligibility = function() {
+export const reimbursementEligibility = function() {
   let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
   let totalBenefitsValue = 0;
   let employeeBenefits = payGrades[getCadre()].benefits;
@@ -41,4 +41,4 @@ const reimbursementEligibility = function() {
   return totalBenefitsValue;
 }
 
-export {Employee, getCadre as cadre, calculateTax as tax, getBenefits as benefits, calculateBonus as bonus, reimbursementEligibility as reimbursement};
+export default Employee;
